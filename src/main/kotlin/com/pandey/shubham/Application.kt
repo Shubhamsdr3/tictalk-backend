12d4b334ec6edccc6fa757c5aeee190f02df8e26
Shubham Pandey <shubham.pandey@1mg.com>
2021-11-18 12:51:52 +0530

    change routing

--- src/main/kotlin/com/pandey/shubham/Application.kt
+++ src/main/kotlin/com/pandey/shubham/Application.kt
@@ -7,42 +7,53 @@ import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
 import io.ktor.features.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.serialization.json.Json
 
-val jwtConfig = JwtConfig("2E3F09EA56DDD11318E367BB820B5693552298F853E3D32756F507A56F04D2CD")
+//val jwtConfig = JwtConfig("2E3F09EA56DDD11318E367BB820B5693552298F853E3D32756F507A56F04D2CD")
 
-fun main() {
-    val server = embeddedServer(Netty, port = 8888) {
-//        DatabaseFactory.initDb()
-        install(DefaultHeaders)
-        install(CallLogging)
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-                ignoreUnknownKeys = true
-            })
-        }
-        install(Authentication) {
-            jwt {
-                jwtConfig.configureKtorFeature(this)
-            }
-        }
-        routing {
-            userRouting()
+//fun main() {
+//    val server = embeddedServer(Netty, port = 8888) {
+////        DatabaseFactory.initDb()
+//        install(DefaultHeaders)
+//        install(CallLogging)
+//        install(ContentNegotiation) {
+//            json(Json {
+//                prettyPrint = true
+//                isLenient = true
+//                ignoreUnknownKeys = true
+//            })
+//        }
+//        install(Authentication) {
+//            jwt {
+//                jwtConfig.configureKtorFeature(this)
+//            }
+//        }
+//        routing {
+//            userRouting()
+//        }
+//    }
+//    server.start(wait = true)
+//}
+//
+//fun Application.module() {
+//    DatabaseFactory.init()
+//    install(ContentNegotiation) {
+//        json()
+//    }
+//}
+
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
         }
     }
-    server.start(wait = true)
 }
-
-fun Application.module() {
-    DatabaseFactory.init()
-    install(ContentNegotiation) {
-        json()
-    }
-}
-
