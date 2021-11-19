051217f4e13db36adbcacc41714977920101a147
Shubham Pandey <shubham.pandey@1mg.com>
2021-11-19 16:47:33 +0530

    added logging

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,15 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
     routing {
         get("/") {
             call.respondText("Hello, world!")
