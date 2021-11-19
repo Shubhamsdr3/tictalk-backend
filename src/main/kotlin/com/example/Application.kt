42f432db6ec9e2ce4d2cec12f89ea53db4a558ec
Shubham Pandey <shubham.pandey@1mg.com>
2021-11-19 10:49:05 +0530

    changed kt dls to gradle

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,15 @@
 package com.example
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import com.example.plugins.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureSecurity()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
 }
